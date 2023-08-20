731dd2de97a4b9577dfd330524ecec9f0ce67e89
zalpader <112233947+zalpader@users.noreply.github.com>
2023-08-20 22:42:23 +0300

    Update BTCZTools.bat
    Added download and uncompress of blockchain backup from the internet
    Added the ability to quickly uncompress your own copy of the blockchain created with BTCZTools

--- BTCZTools.bat
+++ BTCZTools.bat
@@ -1,8 +1,20 @@
-::BTCZTools v1.3b by zalpader
+::BTCZTools v1.4 by zalpader
 @echo off
-set wallet_name=bitcoinz-windows-wallet_2.0.8_win64.zip
+set conf=C:\Users\%USERNAME%\AppData\Roaming\BitcoinZ\bitcoinz.conf
 set version=BitcoinZ Wallet 2.0.8
-set URL=https://github.com/btcz/bitcoinz-wallet/releases/download/v2.0.8/bitcoinz-windows-wallet_2.0.8_win64.zip
+set wallet_name=bitcoinz-windows-wallet_2.0.8_win64.zip
+set wallet_url=https://github.com/btcz/bitcoinz-wallet/releases/download/v2.0.8/bitcoinz-windows-wallet_2.0.8_win64.zip
+set blockchain_folder=C:\Users\%USERNAME%\AppData\Roaming\BitcoinZ
+set blockchain_name_1=BitcoinZ_Blockchain_Backup_2023.07.21_03-59-41.7z
+set blockchain_url_1=https://link.storjshare.io/s/jwtbc6fcdkwmb27xwgju6o3r2y3q/btcz/BitcoinZ_Blockchain_Backup_2023.07.21_03-59-41.7z?download=1
+set blockchain_name_2=BitcoinZ_Blockchain_Backup_2023.04.28_18-57-58.7z
+set blockchain_url_2=https://link.storjshare.io/s/jvdxderwlpxyjfic6ozwjhbrgnqa/btcz/BitcoinZ_Blockchain_Backup_2023.04.28_18-57-58.7z?download=1
+set blockchain_name_3=
+set blockchain_url_3=
+set blockchain_name_4=
+set blockchain_url_4=
+set blockchain_name_5=
+set blockchain_url_5=
 set DATE_TIME=%DATE:~6,4%.%DATE:~3,2%.%DATE:~0,2%_%TIME:~0,2%-%TIME:~3,2%-%TIME:~6,2%
 set DATE_TIME=%DATE_TIME: =0%
 cls
@@ -16,25 +28,38 @@ if exist "C:\Users\%USERNAME%\AppData\Roaming\BitcoinZ-Wallet\bitcoinz-wallet.ex
 echo [1] Uninstall %version%
 echo [2] Uninstall blockchain 
 echo [3] Uninstall all %version% files with blockchain
-echo [4] Backup blockchain
-echo [5] Backup wallet.dat
-echo [6] Backup wallet.dat with a password
-echo [7] Run command line BitcoinZ Node
-echo [8] Reindex blockchain
-echo [9] Rescan TX
-echo [10] Reset 50 backup transactions count message
+echo [4] Download and uncompress blockchain backup
+echo [5] Backup blockchain
+echo [6] Uncompress self blockchain backup
+echo [7] Backup wallet.dat
+echo [8] Backup wallet.dat with a password
+echo [9] Run command line BitcoinZ Node
+echo [10] Reindex blockchain
+echo [11] Rescan TX
+echo [12] Reset 50 backup transactions count message
+echo [13] Fix 0 connections
+
+
 echo.
 set /P input=""
 if %input% == 1 (goto wallet_uninstall)
 if %input% == 2 (goto blockchain_uninstall)
 if %input% == 3 (goto uninstall_all)
-if %input% == 4 (goto blockchain_backup)
-if %input% == 5 (goto wallet_backup)
-if %input% == 6 (goto wallet_pass_backup)
-if %input% == 7 (goto node)
-if %input% == 8 (goto reindex)
-if %input% == 9 (goto rescan_tx)
-if %input% == 10 (goto transactions_count_message)
+if %input% == 4 (goto download_backup)
+if %input% == 5 (goto blockchain_backup)
+if %input% == 6 (goto uncompress_backup)
+if %input% == 7 (goto wallet_backup)
+if %input% == 8 (goto wallet_pass_backup)
+if %input% == 9 (goto node)
+if %input% == 10 (goto reindex)
+if %input% == 11 (goto rescan_tx)
+if %input% == 12 (goto transactions_count_message)
+if %input% == 13 (goto connections)
+
+
+echo.
+echo ERROR! Enter a number from 1 to 13!
+echo.
 goto checking
 
 :menu_2
@@ -48,6 +73,9 @@ echo [1] Install %version%
 echo.
 set /P input=""
 if %input% == 1 (goto wallet_install)
+echo.
+echo ERROR! Enter a number 1!
+echo.
 goto checking
 
 :menu_3
@@ -57,17 +85,26 @@ echo.
 echo [1] Install %version%
 echo [2] Uninstall blockchain 
 echo [3] Uninstall all %version% files with blockchain
-echo [4] Backup blockchain
-echo [5] Backup wallet.dat
-echo [6] Backup wallet.dat with a password
+echo [4] Download and uncompress blockchain backup
+echo [5] Backup blockchain
+echo [6] Uncompress self blockchain backup
+echo [7] Backup wallet.dat
+echo [8] Backup wallet.dat with a password
+echo [9] Fix 0 connections
 echo.
 set /P input=""
 if %input% == 1 (goto wallet_install)
 if %input% == 2 (goto blockchain_uninstall)
 if %input% == 3 (goto uninstall_all)
-if %input% == 4 (goto blockchain_backup)
-if %input% == 5 (goto wallet_backup)
-if %input% == 6 (goto wallet_pass_backup)
+if %input% == 4 (goto download_backup)
+if %input% == 5 (goto blockchain_backup)
+if %input% == 6 (goto uncompress_backup)
+if %input% == 7 (goto wallet_backup)
+if %input% == 8 (goto wallet_pass_backup)
+if %input% == 9 (goto connections)
+echo.
+echo ERROR! Enter a number from 1 to 9!
+echo.
 goto checking
 
 :wallet_install
@@ -84,7 +121,7 @@ echo %version% already installed!
 echo.
 goto checking
 :download
-%cd%\data\aria2c.exe -x 16 -s 16 -k 1M --dir=%cd%\data\ "%URL%"
+%cd%\data\aria2c.exe -x 16 -s 16 -k 1M --dir=%cd%\data "%wallet_url%"
 :unpack
 if exist "C:\Users\%USERNAME%\AppData\Roaming\BitcoinZ-Wallet" (
     goto already_installed
@@ -191,3 +228,170 @@ echo.
 echo Done! It will not bother you for the next 50 transactions!
 echo.
 goto checking
+
+:connections
+echo addnode=seed.btcz.app>> %conf%
+echo addnode=btzseed.blockhub.info>> %conf%
+echo addnode=btzseed2.blockhub.info>> %conf%
+echo addnode=149.28.202.159:1989>> %conf%
+echo addnode=85.237.189.122:1989>> %conf%
+echo addnode=85.15.179.171:1989>> %conf%
+echo addnode=94.237.40.36:1989>> %conf%
+echo addnode=68.195.18.155:1989>> %conf%
+echo addnode=37.187.226.146:1989>> %conf%
+echo addnode=95.216.152.73:1989>> %conf%
+echo addnode=51.75.146.76:1989>> %conf%
+echo addnode=46.4.102.69:1989>> %conf%
+echo addnode=173.212.253.221:1989>> %conf%
+echo addnode=95.216.209.62:1989>> %conf%
+echo addnode=31.40.218.121:1989>> %conf%
+echo.
+echo 15 nodes have been added to the file %conf%
+echo Done! Launch the %version% and check if there is a connection now!
+echo.
+goto checking
+
+:uncompress_backup
+echo.
+echo WARNING! Only 7z files made in the BTCZTools utility are accepted!
+echo.
+set /p user_file="Drag and drop the 7z backup file into this window and press enter: "
+echo.
+if exist "%blockchain_folder%\debug.log" del /f "%blockchain_folder%\debug.log"
+if exist "%blockchain_folder%\blocks" rmdir /S /Q "%blockchain_folder%\blocks"
+if exist "%blockchain_folder%\chainstate" rmdir /S /Q "%blockchain_folder%\chainstate"
+%cd%\data\7z.exe x %user_file% -o"%blockchain_folder%"
+echo.
+goto checking
+
+:download_backup
+echo.
+echo WARNING! Using this backup will significantly accelerate the synchronization process of your wallet and also address certain errors, such as "Activating the best chain" or other blockchain-related issues. However, it's important to note that the decision to trust this backup of the blockchain or to independently perform a full blockchain synchronization rests entirely with you!
+echo.
+echo [0] Return to the main menu
+echo.
+echo Select a file to download and press enter
+echo.
+if "%blockchain_name_1%"=="" (
+  rem
+) else (
+  echo [1] Download %blockchain_name_1%
+)
+if "%blockchain_name_2%"=="" (
+  rem
+) else (
+  echo [2] Download %blockchain_name_2%
+)
+if "%blockchain_name_3%"=="" (
+  rem
+) else (
+  echo [3] Download %blockchain_name_3%
+)
+if "%blockchain_name_4%"=="" (
+  rem
+) else (
+  echo [4] Download %blockchain_name_4%
+)
+if "%blockchain_name_5%"=="" (
+  rem
+) else (
+  echo [5] Download %blockchain_name_5%
+)
+echo.
+set /P input=""
+if %input% == 0 (goto checking)
+if %input% == 1 (goto dl_backup_1)
+if %input% == 2 (goto dl_backup_2)
+if %input% == 3 (goto dl_backup_3)
+if %input% == 4 (goto dl_backup_4)
+if %input% == 5 (goto dl_backup_5)
+echo.
+echo ERROR! Enter a number from 0 to 5!
+echo.
+goto download_backup
+
+:dl_backup_1
+if "%blockchain_url_1%"=="" (
+  goto errors
+)
+if exist "%cd%\data\%blockchain_name_1%" (
+    goto unpack_backup_1
+)
+%cd%\data\aria2c.exe -x 16 -s 16 -k 1M --file-allocation=none --dir=%cd%\data "%blockchain_url_1%"
+:unpack_backup_1
+if exist "%blockchain_folder%\debug.log" del /f "%blockchain_folder%\debug.log"
+if exist "%blockchain_folder%\blocks" rmdir /S /Q "%blockchain_folder%\blocks"
+if exist "%blockchain_folder%\chainstate" rmdir /S /Q "%blockchain_folder%\chainstate"
+%cd%\data\7z.exe x "%cd%\data\%blockchain_name_1%" -o"%blockchain_folder%"
+echo.
+goto checking
+
+:dl_backup_2
+if "%blockchain_url_2%"=="" (
+  goto errors
+)
+if exist "%cd%\data\%blockchain_name_2%" (
+    goto unpack_backup_2
+)
+%cd%\data\aria2c.exe -x 16 -s 16 -k 1M --file-allocation=none --dir=%cd%\data "%blockchain_url_2%"
+:unpack_backup_2
+if exist "%blockchain_folder%\debug.log" del /f "%blockchain_folder%\debug.log"
+if exist "%blockchain_folder%\blocks" rmdir /S /Q "%blockchain_folder%\blocks"
+if exist "%blockchain_folder%\chainstate" rmdir /S /Q "%blockchain_folder%\chainstate"
+%cd%\data\7z.exe x "%cd%\data\%blockchain_name_2%" -o"%blockchain_folder%"
+echo.
+goto checking
+
+:dl_backup_3
+if "%blockchain_url_3%"=="" (
+  goto errors
+)
+if exist "%cd%\data\%blockchain_name_3%" (
+    goto unpack_backup_3
+)
+%cd%\data\aria2c.exe -x 16 -s 16 -k 1M --file-allocation=none --dir=%cd%\data "%blockchain_url_3%"
+:unpack_backup_3
+if exist "%blockchain_folder%\debug.log" del /f "%blockchain_folder%\debug.log"
+if exist "%blockchain_folder%\blocks" rmdir /S /Q "%blockchain_folder%\blocks"
+if exist "%blockchain_folder%\chainstate" rmdir /S /Q "%blockchain_folder%\chainstate"
+%cd%\data\7z.exe x "%cd%\data\%blockchain_name_3%" -o"%blockchain_folder%"
+echo.
+goto checking
+
+:dl_backup_4
+if "%blockchain_url_4%"=="" (
+  goto errors
+)
+if exist "%cd%\data\%blockchain_name_4%" (
+    goto unpack_backup_4
+)
+%cd%\data\aria2c.exe -x 16 -s 16 -k 1M --file-allocation=none --dir=%cd%\data "%blockchain_url_4%"
+:unpack_backup_4
+if exist "%blockchain_folder%\debug.log" del /f "%blockchain_folder%\debug.log"
+if exist "%blockchain_folder%\blocks" rmdir /S /Q "%blockchain_folder%\blocks"
+if exist "%blockchain_folder%\chainstate" rmdir /S /Q "%blockchain_folder%\chainstate"
+%cd%\data\7z.exe x "%cd%\data\%blockchain_name_4%" -o"%blockchain_folder%"
+echo.
+goto checking
+
+:dl_backup_5
+if "%blockchain_url_5%"=="" (
+  goto errors
+)
+if exist "%cd%\data\%blockchain_name_5%" (
+    goto unpack_backup_5
+)
+%cd%\data\aria2c.exe -x 16 -s 16 -k 1M --file-allocation=none --dir=%cd%\data "%blockchain_url_5%"
+:unpack_backup_5
+if exist "%blockchain_folder%\debug.log" del /f "%blockchain_folder%\debug.log"
+if exist "%blockchain_folder%\blocks" rmdir /S /Q "%blockchain_folder%\blocks"
+if exist "%blockchain_folder%\chainstate" rmdir /S /Q "%blockchain_folder%\chainstate"
+%cd%\data\7z.exe x "%cd%\data\%blockchain_name_5%" -o"%blockchain_folder%"
+echo.
+goto checking
+
+:errors
+echo.
+echo Oops Something went wrong
+echo.
+goto download_backup
